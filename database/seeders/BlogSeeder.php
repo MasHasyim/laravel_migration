ed6630e2e1e511fc50879f2a7ae5a21b76f3bf9a
MasHasyim <fikri.huffadz@gmail.com>
2025-02-27 16:07:18 +0700

    seedr & make dummy data

---
+++ database/seeders/BlogSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Blog;
+use App\Models\User;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class BlogSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+
+        DB::table('blogs')->truncate();
+
+        Blog::factory()->count(10)->create(); //buat bikin data dummy
+
+        // DB::table('blogs')->insert([
+        //     'title' => 'Nadia',
+        //     'description' => 'Cantik nyoooo'
+        // ]);
+
+        // DB::table('blogs')->insert([
+        //     'title' => 'Fikri',
+        //     'description' => 'Ganteng nyoooo'
+        // ]);
+
+        // DB::table('blogs')->insert([
+        //     'title' => 'Haashim',
+        //     'description' => 'Ganteng nyoooo'
+        // ]);
+    }
+}
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->truncate();
+
+        DB::table('users')->insert([
+            'name' => 'Nadia',
+            'email' => 'Cantik@nyoooo',
+            'password' => '1234'
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'Fikri',
+            'email' => 'Ganteng@nyoooo',
+            'password' => '123'
+
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'Haashim',
+            'email' => 'Ganteng@nyoo',
+            'password' => '12'
+
+        ]);
+    }
+}
